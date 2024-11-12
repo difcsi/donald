7b8e4853e29199f07047e5810b48e36e56c69942
Stephen Kell <srk31@srcf.ucam.org>
2024-11-12 03:50:28 +0000

    debug.c: sketch experiment with early debugging of inferior
    The idea here is to install fake r_debug and link_map structures in the
    created DT_DEBUG entry in the chain loader, so that debugging can work
    during the early execution of the inferior ld.so. It will eventually
    clobber the DT_DEBUG with its own initializers. The main piece of code
    here is #ifdef'd out because it causes glibc's ld.so to crash... need to
    understand why.
    
    Conflicts:
    	src/debug.c

--- src/debug.c
+++ src/debug.c
@@ -10,6 +10,7 @@
 #include <assert.h>
 #include <string.h> /* for strcmp() */
 #include <linux/auxvec.h>  /* For AT_xxx definitions */
+#include <link.h>
 #include "donald.h"
 
 /* The purpose of this code is to set the *executable's* DT_DEBUG entry
@@ -47,5 +48,36 @@ ElfW(Dyn) *find_or_create_dt_debug(uintptr_t inferior_load_addr, uintptr_t infer
 	// make *our* _DYNAMIC point to the *inferior*'s _r_debug
 	d->d_un.d_ptr = (uintptr_t) r;
 	return d;
+}
+
+__attribute__((visibility("hidden")))
+void populate_dt_debug(ElfW(Dyn) *d, uintptr_t inferior_load_addr,
+	uintptr_t inferior_dynamic_vaddr, uintptr_t inferior_r_debug_vaddr)
+{
+	struct r_debug *r = (struct r_debug *)(inferior_load_addr + inferior_r_debug_vaddr);
+	/* What about the contents of the inferior's r_debug? To enable
+	 * debugging early in the ld.so, we could create a fake entry. Let's try it. */
+	static struct link_map fake_link_map = {
+		.l_addr = 0 /* inferior_load_addr */,
+		.l_name = SYSTEM_LDSO_PATH,
+		.l_ld = 0 /* (ElfW(Dyn) *) inferior_dynamic_vaddr */,
+		.l_prev = NULL,
+		.l_next = NULL
+	};
+	extern void _dl_debug_state(void);
+	fake_link_map.l_addr = inferior_load_addr;
+	fake_link_map.l_ld = (ElfW(Dyn) *) (inferior_load_addr + inferior_dynamic_vaddr);
+	// FIXME: this causes glibc's ld.so to jump to (void*)-1... understand why
+#if 0
+	*r = (struct r_debug) {
+		.r_version = 1,
+		.r_map = &fake_link_map,
+		.r_brk = (uintptr_t) _dl_debug_state,
+		.r_state = RT_CONSISTENT,
+		.r_ldbase = inferior_load_addr
+	};
+#endif
+	_dl_debug_state(); // trigger the attached debugger, if any
+//#endif
 }
 #endif
